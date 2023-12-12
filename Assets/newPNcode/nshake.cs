6748493ac703e94c398a6e5cdfc837d949f05b1f
kutoriri <b2201920@photon.chitose.ac.jp>
2023-12-12 18:18:15 +0900

    電子の動き方迷走中

---
+++ Assets/newPNcode/nshake.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class nshake : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("FunctionTest", 0f, 1.0f);
+    }
+
+    void FunctionTest()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(transform.up * 5.0f, ForceMode.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
