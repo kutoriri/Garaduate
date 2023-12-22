51eb83e2bbbd19e0b1f29d80582212fcac76c7df
kutoriri <b2201920@photon.chitose.ac.jp>
2023-12-22 10:31:41 +0900

    d途中

--- Assets/Diaodcode/DMovingBand.cs
+++ Assets/Diaodcode/DMovingBand.cs
@@ -5,16 +5,16 @@ using UnityEngine;
 public class DMovingBand : MonoBehaviour
 {
     private bool isMoving = false;
-    [SerializeField] private float x = 0.0f;
-    [SerializeField] private float y = 0.0f;
-    [SerializeField] private float z = 0.0f;
-    // -0.01f, 4.75f, -2.8f
+    public float x = 0f;
+    public float y = 2.5f;
+    public float z = 9.0f;
+    public float MT = 6.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        isMoving = false;
-        Invoke("Call", 2);
+
+        Invoke("Call", MT);
     }
 
     void Call()
@@ -28,7 +28,7 @@ public class DMovingBand : MonoBehaviour
         if (isMoving == false) return;
         Vector3 current = transform.position;
         Vector3 target = new Vector3(x, y, z);
-        float step = 2.0f * Time.deltaTime;
-        transform.position = Vector3.MoveTowards(current, target, Time.deltaTime * 0.8f);
+        //float step = 2.0f * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(current, target, Time.deltaTime * 2.5f);
     }
 }
---
+++ Assets/Diaodcode/DNewEandH.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DNewEandH : MonoBehaviour
+{
+    [SerializeField] private GameObject a;
+    public float ET = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("Call", ET);
+    }
+
+    void Call()
+    {
+        a.SetActive(true);
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
