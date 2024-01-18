6fe7d761c198007c11b202c686543bf5508a1b73
kutoriri <b2201920@photon.chitose.ac.jp>
2024-01-18 17:08:11 +0900

    a

---
+++ Assets/Moscode/AddForceRepeat.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddForceRepeat : MonoBehaviour
+{
+    public float Time = 0f;
+    public float Time2 = 1.0f;
+    public float xpower = 1.0f;
+    public float ypower = 1.0f;
+    public float zpower = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Call",Time , Time2);
+        //Invoke("Call", Time);
+    }
+
+    void Call()
+    {
+        Rigidbody rb = this.GetComponent<Rigidbody>();  // rigidbodyを取得
+        Vector3 force = new Vector3(xpower, ypower, zpower);    // 力を設定
+        rb.AddForce(force, ForceMode.Impulse);  // 力を加える
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Moscode/MobjectScale.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobjectScale : MonoBehaviour
+{
+    public float MT = 4.0f;
+    public float scale = 3.0f;
+    public float x = 6.0f;
+    public float y = 6.0f;
+    public float z = 0.2f;
+
+    //private bool isMoving = false;
+
+    void Start()
+    {
+        Invoke("Call", MT);
+        //StartCoroutine("ScaleUp");
+    }
+
+    void Call()
+    {
+        StartCoroutine("ScaleUp");
+    }
+
+    IEnumerator ScaleUp()
+    {
+        //if (isMoving == false) return;
+
+        for (float i = 0.1f; i < scale; i += 0.05f)
+        {
+            this.transform.localScale = new Vector3(x, y - i, z);
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
